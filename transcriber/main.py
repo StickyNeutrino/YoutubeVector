932e362fb651f27bd864299d4c8e4f0dfdfcc135
stickyneutrino <dfitc001@ucr.edu>
2020-05-25 00:35:14 +0000

    Bug fixes and verbosity

--- transcriber/main.py
+++ transcriber/main.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient, errors
 from pocketsphinx import AudioFile, get_model_path, get_data_path, Pocketsphinx
 from time import sleep
 from os import listdir, path
-from subprocess import run
+from subprocess import run, DEVNULL
 
 print('started')
 
@@ -30,10 +30,9 @@ while 1:
             print('transcribing:', id)
             try:
                 videos.update_one({'id': video['id']} , {'$set': {'text': '*'}})
-                run(['ffmpeg', '-y', '-i', './videos/' + id + '.mp4', '-acodec', 'pcm_s16le', '-f', 's16le', '-ac', '1', '-ar', '16000', './tmp.raw'])
-                text = transcribe('tmp.raw')
+                run(['ffmpeg', '-y', '-i', './videos/' + id + '.mp4', '-acodec', 'pcm_s16le', '-f', 's16le', '-ac', '1', '-ar', '16000', './tmp.raw'], stderr=DEVNULL, stdout=DEVNULL)
+                text = transcribe('./tmp.raw')
                 videos.update_one({'id': video['id']} , {'$set': {'text': text}})
-                print(text)
             except:
                 print('skipping:', id)
                 skipList.append(id)
