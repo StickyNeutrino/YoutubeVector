c0cc58cae578061edf03e91c56065125c967bb53
stickyneutrino <dfitc001@ucr.edu>
2020-06-04 04:16:17 +0000

    Cleaned up main loop logic

--- transcriber/main.py
+++ transcriber/main.py
@@ -10,26 +10,28 @@ client = MongoClient(
     password = "example")
 
 videos = client.youtube.videos
+transcripts = client.youtube.transcripts
 
 def transcribe(audiofile):
     return Pocketsphinx()\
         .decode( audio_file = audiofile)\
         .hypothesis()
 
-skipList = []
-def run():
-    for video in videos.find({'text': None}):
+def videoExists(id):
+    id + '.mp4' in listdir('./videos')
+
+def transcribed(id):
+    transcripts.find({'id': id}, {'id': 1}).limit(1).count() != 0
+    #do simple cache here
+while 1:
+    for video in videos.find({}, {'yt_videoid': 1}):
         id = video['yt_videoid']
-        if id + '.mp4' in listdir('./videos') \
-        and id not in skipList:
+        if not transcribed(id) \
+        and videoExists(id):
             try:
                 run(['ffmpeg', '-y', '-i', './videos/' + id + '.mp4', '-acodec', 'pcm_s16le', '-f', 's16le', '-ac', '1', '-ar', '16000', './tmp.raw'], stderr=DEVNULL, stdout=DEVNULL)
                 text = transcribe('./tmp.raw')
                 videos.update_one({'id': video['id']} , {'$set': {'text': text}})
             except:
-                skipList.append(id)
-
-
-while 1:
-    run()
+                continue
     sleep(10)
