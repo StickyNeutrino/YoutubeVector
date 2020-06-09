20e0223d527ec7914469dff6c4cdf550172248b6
stickyneutrino <dfitc001@ucr.edu>
2020-06-09 00:12:23 +0000

    Cache transcripts in mongodb

--- pipeline/main.py
+++ pipeline/main.py
@@ -7,12 +7,23 @@ client = MongoClient(
     'mongodb://mongo:27017/',
     username="root", password="example")
 
+transcripts = client.youtube.transcripts
+
 app = FastAPI()
 
 @app.get("/video2vec/{video_id}")
 def pipeline_endpoint(video_id: str):
     requests.get('http://downloader:8000/download/' + video_id)
-    resp = requests.get('http://transcriber:8000/transcribe/' + video_id)
-    text = resp.json().text
+
+    transcript = transcripts.find_one({'id': video_id})
+    text = ''
+    if not transcript:
+        resp = requests.get('http://transcriber:8000/transcribe/' + video_id)
+        value = resp.json()
+        transcripts.insert({ 'id': video_id, 'value': value})
+        text = value['text']
+    else:
+        text = transcript['value']['text']
+
     print(text)
     return { 'success': True }
